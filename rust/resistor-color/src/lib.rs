4d320093898348f214e9a97b230b0d4beef3d3a6
Faris Alotaibi <faa339@nyu.edu>
2022-05-19 09:22:12 -0400

    im learning bossgit add .

---
+++ rust/resistor-color/src/lib.rs
@@ -0,0 +1,32 @@
+use int_enum::IntEnum;
+use enum_iterator::IntoEnumIterator;
+#[repr(usize)]
+#[derive(Debug, PartialEq,Copy,Clone, IntEnum, IntoEnumIterator)]
+pub enum ResistorColor {
+    Black  = 0,
+    Brown  = 1,
+    Red    = 2,
+    Orange = 3,
+    Yellow = 4,
+    Green  = 5,
+    Blue   = 6,
+    Violet = 7,
+    Grey   = 8,
+    White  = 9,
+}
+
+pub fn color_to_value(_color: ResistorColor) -> usize {
+    return _color.int_value();
+}
+
+pub fn value_to_color_string(value: usize) -> String {
+    let mid = ResistorColor::from_int(value);
+    match mid{
+        Ok(v)  => return format!("{:?}", v),
+        _      => return "value out of range".to_string(),
+    }
+}
+
+pub fn colors() -> Vec<ResistorColor> {
+    return Vec::from_iter(ResistorColor::into_enum_iter());
+}
--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -5,7 +5,24 @@ pub enum Comparison {
     Superlist,
     Unequal,
 }
-//in prog on this one, taking a break
+//thank you to the community solution from alireza4050 -- dont think 
+//i would have known how array windows worked without it.
+//found here https://exercism.org/tracks/rust/exercises/sublist/solutions/alireza4050
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-
+    let a = _first_list;
+    let b = _second_list;
+    match(a.len(), b.len()){
+        //these first 3 are obvious -- matching lengths
+        (0,0)        => Comparison::Equal,
+        (0,_)        => Comparison::Sublist,
+        (_,0)        => Comparison::Superlist,
+        //these next 2 are a little special -- functional style workings here 
+        //windows gives you an iterator that goes over the array in groupings of size n -- [1,2,3,4].windows(2)
+        //gives you [1,2], [2,3], [3,4], for example. any is just a search based on a predicate. 
+        //runtime wise, this takes however long it takes to make the predicate + at least O(n) 
+        //since the window searching will always be linear
+        (m,n) if m>n => if a.windows(n).any(|v| v==b) {Comparison::Superlist} else {Comparison::Unequal},
+        (m,n) if m<n => if b.windows(m).any(|v| v==a) {Comparison::Sublist}   else {Comparison::Unequal},
+        (_,_)        => if a==b {Comparison::Equal} else {Comparison::Unequal},
+    }
 }
