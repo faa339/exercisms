bdf61a756535da21cd0d8d2c88a3b218a7affdf3
Faris Alotaibi <faa339@nyu.edu>
2022-05-21 17:47:44 -0400

    fixing up stuff so that clippy is happy, adding other files too

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,32 +1,33 @@
 use std::fmt;
 
 #[derive(Debug, Eq, PartialEq)]
-pub struct Clock{
+pub struct Clock {
     minutes: i32,
 }
-//tried working with hours and mins before, this ended up being way simpler 
-//we take advantage of the fact that there are 1440 mins per day, 
-//then just add mins and use rem_euclid to mod it and handle negatives 
+//tried working with hours and mins before, this ended up being way simpler
+//we take advantage of the fact that there are 1440 mins per day,
+//then just add mins and use rem_euclid to mod it and handle negatives
 //we only care about formatting when displaying so we do that at display time
-const DAY_MINS:i32 = 24*60;
+const DAY_MINS: i32 = 24 * 60;
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
         let m = (hours * 60 + minutes).rem_euclid(DAY_MINS);
-        return Clock{minutes:m};
+        Clock { minutes: m }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        return Clock{minutes: (self.minutes + minutes).rem_euclid(DAY_MINS)}
+        Clock {
+            minutes: (self.minutes + minutes).rem_euclid(DAY_MINS),
+        }
     }
 
-    pub fn to_string(&self) -> String{
-        return format!("{:02}:{:02}", self.minutes/60, self.minutes%60);
-    }
+    //     pub fn to_string(&self) -> String{
+    //         format!("{:02}:{:02}", self.minutes/60, self.minutes%60)
+    //     }
 }
 
-impl fmt::Display for Clock{
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result{
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{:02}:{:02}", self.minutes / 60, self.minutes % 60)
     }
 }
-
